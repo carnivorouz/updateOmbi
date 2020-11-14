81e50b72d7d90378a23e21ef9390215e2c90a1ab
didyouexpectthat <didyouexpectthat@users.noreply.github.com>
2020-11-13 21:09:25 -0800

    Update to work on my computer
    Fix several syntax

--- updateOmbi.sh
+++ updateOmbi.sh
@@ -5,9 +5,10 @@ SERVICE_NAME=ombi
 VERSION=$(curl -s https://github.com/tidusjar/$SERVICE_NAME.releases/releases | grep "$DOWNLOAD" | grep -Po ".*\/download\/v([0-9\.]+).*" | awk -F'/' '{print $6}' | tr -d 'v' | sort -n | tail -1)
 SERVICE_LOC=$(systemctl status $SERVICE_NAME | grep -Po "(?<=loaded \()[^;]+")
 WORKING_DIR=$(grep -Po "(?<=WorkingDirectory=).*" $SERVICE_LOC)
-INSTALLED=$(strings $WORKING_DIR/Ombi | grep -Po 'Ombi/\d+\.\d+\.\d+' | grep -Po '\d+\.\d+\.\d+' | sort -n | tail -n 1)
-BACKUP_DIR=$WORKING_DIR.$INSTALLED
-TEMP_DIR=$WORKING_DIR.$VERSION
+INSTALLED=$(grep Ombi/4 $WORKING_DIR/Ombi.deps.json | head -n 1 | sed 's/.*"Ombi\///;s/": {//')
+#INSTALLED=$(strings $WORKING_DIR/Ombi | grep -Po 'Ombi/\d+\.\d+\.\d+' | grep -Po '\d+\.\d+\.\d+' | sort -n | tail -n 1)
+BACKUP_DIR=$WORKING_DIR../.$INSTALLED
+TEMP_DIR=$WORKING_DIR../.$VERSION
 URL=https://github.com/tidusjar/Ombi.Releases/releases/download/v
 KEEP_BACKUP=no
 SLACK_URL=https://hooks.slack.com/services/
@@ -19,21 +20,21 @@ SLACK_USER=ombi
 # Start script
 if [ "$INSTALLED" = "$VERSION" ]; then
         echo "$TIMESTAMP $SERVICE_NAME is up to date"
-	exit 0
+        exit 0
  else
         echo "$TIMESTAMP Updating $SERVICE_NAME"
-	curl -X POST --data "payload={\"channel\": \"#$SLACK_CHANNEL\", \"username\": \"$SLACK_USER\", \"text\": \":exclamation: ${SLACK_MESSAGE} \"}" $SLACK_URL$SLACK_WEBHOOK
+        curl -X POST --data "payload={\"channel\": \"#$SLACK_CHANNEL\", \"username\": \"$SLACK_USER\", \"text\": \":exclamation: ${SLACK_MESSAGE} \"}" $SLACK_URL$SLACK_WEBHOOK
 fi
 
 echo  "$TIMESAMP Stopping $SERVICE_NAME"
 systemctl stop $SERVICE_NAME
 
 echo "$TIMESTAMP Creating temporary directory $TEMP_DIR"
-mkdir $TEMP_DIR
+mkdir $TEMP_DIR $BACKUP_DIR
 cd $TEMP_DIR
 
 echo "$TIMESTAMP Downloading $SERVICE_NAME"
-wget $URL$VERSION/$DOWNLOAD
+wget $URL$VERSION/$DOWNLOAD -O $DOWNLOAD
 
 if [ $? -ne 0 ]; then
    echo "$TIMESTAMP Failed to download"
@@ -41,11 +42,11 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "$TIMESTAMP Extracting $DOWNLOAD"
-tar -xf $DOWNLOAD
+tar -xf "$DOWNLOAD"
 
 echo "$TIMESTAMP Shuffling directories"
-mv $WORKING_DIR $BACKUP_DIR
-mv $TEMP_DIR $WORKING_DIR
+mv $WORKING_DIR/* $BACKUP_DIR/
+mv $TEMP_DIR/* $WORKING_DIR/
 
 echo "$TIMESTAMP Copying over files from $BACKUP_DIR"
 if [ -f $BACKUP_DIR/database.json ]; then
