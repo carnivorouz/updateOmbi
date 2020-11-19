75a25e48873eb87d3af962d584b712a4afdbe960
carnivorouz <mullins.chase@gmail.com>
2020-11-19 03:05:54 -0600

    Changed how timestamps are represented

--- updateOmbi.sh
+++ updateOmbi.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-TIMESTAMP=$(date +"%Y-%m-%e %l:%M:%S %t")
 DOWNLOAD=linux-x64.tar.gz
 SERVICE_NAME=ombi
 VERSION=$(curl -s https://github.com/tidusjar/$SERVICE_NAME.releases/releases | grep "$DOWNLOAD" | grep -Po ".*\/download\/v([0-9\.]+).*" | awk -F'/' '{print $6}' | tr -d 'v' | sort -n | tail -1)
@@ -19,47 +18,47 @@ SLACK_USER=ombi
 
 # Start script
 if [ ! -z "$INSTALLED_1" ]; then
-        echo "$TIMESTAMP $SERVICE_NAME $INSTALLED_1 detected. Continuing..."
+        echo "$(date +"%Y-%m-%e %l:%M:%S %t") $SERVICE_NAME $INSTALLED_1 detected. Continuing..."
         INSTALLED=$INSTALLED_1
 elif [ ! -z "$INSTALLED_2" ]; then
-        echo "$TIMESTAMP $SERVICE_NAME $INSTALLED_2 detected. Continuing..."
+        echo "$(date +"%Y-%m-%e %l:%M:%S %t") $SERVICE_NAME $INSTALLED_2 detected. Continuing..."
         INSTALLED=$INSTALLED_2
 else
-        echo "$TIMESTAMP $SERVICE_NAME version not detected. Exiting."
+        echo "$(date +"%Y-%m-%e %l:%M:%S %t") $SERVICE_NAME version not detected. Exiting."
         exit 1
 fi
 
 if [ "$INSTALLED" = "$VERSION" ]; then
-        echo "$TIMESTAMP $SERVICE_NAME is up to date"
+        echo "$(date +"%Y-%m-%e %l:%M:%S %t") $SERVICE_NAME is up to date"
 	exit 0
  else
-        echo "$TIMESTAMP Updating $SERVICE_NAME"
+        echo "$(date +"%Y-%m-%e %l:%M:%S %t") Updating $SERVICE_NAME"
 	curl -X POST --data "payload={\"channel\": \"#$SLACK_CHANNEL\", \"username\": \"$SLACK_USER\", \"text\": \":exclamation: ${SLACK_MESSAGE} \"}" $SLACK_URL$SLACK_WEBHOOK
 fi
 
-echo  "$TIMESAMP Stopping $SERVICE_NAME"
+echo  "$(date +"%Y-%m-%e %l:%M:%S %t") Stopping $SERVICE_NAME"
 systemctl stop $SERVICE_NAME
 
-echo "$TIMESTAMP Creating temporary directory $TEMP_DIR"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Creating temporary directory $TEMP_DIR"
 mkdir $TEMP_DIR
 cd $TEMP_DIR
 
-echo "$TIMESTAMP Downloading $SERVICE_NAME"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Downloading $SERVICE_NAME"
 wget $URL$VERSION/$DOWNLOAD
 
 if [ $? -ne 0 ]; then
-   echo "$TIMESTAMP Failed to download"
+   echo "$(date +"%Y-%m-%e %l:%M:%S %t") Failed to download"
    exit 1
 fi
 
-echo "$TIMESTAMP Extracting $DOWNLOAD"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Extracting $DOWNLOAD"
 tar -xf $DOWNLOAD
 
-echo "$TIMESTAMP Shuffling directories"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Shuffling directories"
 mv $WORKING_DIR $BACKUP_DIR
 mv $TEMP_DIR $WORKING_DIR
 
-echo "$TIMESTAMP Copying over files from $BACKUP_DIR"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Copying over files from $BACKUP_DIR"
 if [ -f $BACKUP_DIR/database.json ]; then
   cp $BACKUP_DIR/database.json $WORKING_DIR
 fi
@@ -81,15 +80,15 @@ if [ -f $BACKUP_DIR/OmbiSettings.db ]; then
 fi
 cp -rn $BACKUP_DIR/wwwroot/images/* $WORKING_DIR/wwwroot/images || true
 
-echo "$TIMESTAMP Changing ownership to $SERVICE_NAME"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Changing ownership to $SERVICE_NAME"
 chown -R $SERVICE_NAME:$SERVICE_NAME $WORKING_DIR
 
 if [ $KEEP_BACKUP == "yes" ]; then
-   echo "$TIMESTAMP Keeping $BACKUP_DIR"
+   echo "$(date +"%Y-%m-%e %l:%M:%S %t") Keeping $BACKUP_DIR"
 elif [ $KEEP_BACKUP == "no" ]; then
-   echo "$TIMESTAMP Deleting $BACKUP_DIR"
+   echo "$(date +"%Y-%m-%e %l:%M:%S %t") Deleting $BACKUP_DIR"
    rm -rf $BACKUP_DIR
 fi
 
-echo "$TIMESTAMP Starting $SERVICE_NAME"
+echo "$(date +"%Y-%m-%e %l:%M:%S %t") Starting $SERVICE_NAME"
 systemctl start $SERVICE_NAME
