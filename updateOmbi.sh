a7e139448d3f33e30f6e09e0d653dc2bee36decc
carnivorouz <mullins.chase@gmail.com>
2021-08-17 22:01:48 -0500

    'strings' command check
    Add a check for the 'strings' command that is necessary.

--- updateOmbi.sh
+++ updateOmbi.sh
@@ -8,10 +8,10 @@ WORKING_DIR=$(grep -Po "(?<=WorkingDirectory=).*" $SERVICE_LOC)
 INSTALLED_1=$(strings $WORKING_DIR/Ombi | grep -Po 'Ombi/\d+\.\d+\.\d+' | grep -Po '\d+\.\d+\.\d+' | sort -n | tail -n 1)
 INSTALLED_2=$(grep -Po "(?<=Ombi/)([\d\.]+)" 2> /dev/null $WORKING_DIR/Ombi.deps.json | head -1)
 STORAGE_DIR=
-KEEP_BACKUP=yes
+KEEP_BACKUP=no
 SUPPRESS_OUTPUT=no
 SLACK_URL=https://hooks.slack.com/services/
-SLACK_WEBHOOK=
+SLACK_WEBHOOK=T4LRPDYTE/B4LSP537W/NvNlYWErWrjtcBgYM7RuX27r
 MESSAGE="Updating $SERVICE_NAME to v$VERSION"
 SLACK_CHANNEL=alerts
 SLACK_USER=ombi
@@ -20,60 +20,67 @@ DISCORD_WEBHOOK=
 # Start script
 # Privileges check
 if [ "$EUID" -ne 0 ]; then
-	if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [WARNING] Not running as root. You must have your sudoers file configured correctly."
-	fi
+        if [ $SUPPRESS_OUTPUT = 'no' ]; then
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [WARNING] Not running as root. You must have your sudoers file configured correctly."
+        fi
+fi
+
+# Check for the 'strings' command
+if strings --help > /dev/null 2>&1 ; then
+        :
+else
+        echo "Command 'strings' not found. This package needs to be installed through 'sudo dnf install bintuils' or 'sudo apt install binutils'"
 fi
 
 # Check for version info in the executable
 if [ ! -z "$INSTALLED_1" ]; then
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] $SERVICE_NAME v$INSTALLED_1 detected. Continuing..."
-	fi
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] $SERVICE_NAME v$INSTALLED_1 detected. Continuing..."
+        fi
         INSTALLED=$INSTALLED_1
 # Check for version info before it was in the executable
 elif [ ! -z "$INSTALLED_2" ]; then
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") $SERVICE_NAME v$INSTALLED_2 [INFO] detected. Continuing..."
-	fi
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") $SERVICE_NAME v$INSTALLED_2 [INFO] detected. Continuing..."
+        fi
         INSTALLED=$INSTALLED_2
 else
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] Currently installed version of $SERVICE_NAME not detected. Exiting."
-	fi
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] Currently installed version of $SERVICE_NAME not detected. Exiting."
+        fi
         exit 1
 fi
 
 if [ "$INSTALLED" = "$VERSION" ]; then
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] $SERVICE_NAME is up to date"
-	fi
-	exit 0
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] $SERVICE_NAME is up to date"
+        fi
+        exit 0
 elif [ -z $VERSION ]; then
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] Latest version of $SERVICE_NAME not found. Exiting."
-	fi
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] Latest version of $SERVICE_NAME not found. Exiting."
+        fi
         exit 1
 else
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Updating $SERVICE_NAME to v$VERSION"
-	fi
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Updating $SERVICE_NAME to v$VERSION"
+        fi
 
 # POST to Slack
-	curl -s -o /dev/null -X POST --data "payload={\"channel\": \"#$SLACK_CHANNEL\", \"username\": \"$SLACK_USER\", \"text\": \":exclamation: ${MESSAGE} \"}" $SLACK_URL$SLACK_WEBHOOK
+        curl -s -o /dev/null -X POST --data "payload={\"channel\": \"#$SLACK_CHANNEL\", \"username\": \"$SLACK_USER\", \"text\": \":exclamation: ${MESSAGE} \"}" $SLACK_URL$SLACK_WEBHOOK
 # POST to Discord
-	curl -s -o /dev/null -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $DISCORD_WEBHOOK
+        curl -s -o /dev/null -H "Content-Type: application/json" -X POST -d "{\"content\": \"$MESSAGE\"}" $DISCORD_WEBHOOK
 fi
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo  "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Stopping $SERVICE_NAME"
+        echo  "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Stopping $SERVICE_NAME"
 fi
 
 systemctl stop $SERVICE_NAME
 if [ $? = 1 ]; then
         if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] User does not have the permission to control $SERVICE_NAME service"
-	fi
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] User does not have the permission to control $SERVICE_NAME service"
+        fi
         exit 1
 fi
 
@@ -88,31 +95,31 @@ BACKUP_DIR=$WORKING_DIR.$INSTALLED
 TEMP_DIR=$WORKING_DIR.$VERSION
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Creating temporary directory $TEMP_DIR"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Creating temporary directory $TEMP_DIR"
 fi
 mkdir $TEMP_DIR
 cd $TEMP_DIR
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Downloading $SERVICE_NAME"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Downloading $SERVICE_NAME"
 fi
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	wget -N $OMBI_URL/download/v$VERSION/$DOWNLOAD
+        wget -N $OMBI_URL/download/v$VERSION/$DOWNLOAD
 else
-	wget -Nq $OMBI_URL/download/v$VERSION/$DOWNLOAD
+        wget -Nq $OMBI_URL/download/v$VERSION/$DOWNLOAD
 fi
 
 if [ $? -ne 0 ]; then
-	if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] Failed to download"
-		rm -f $DOWNLOAD
-	fi
+        if [ $SUPPRESS_OUTPUT = 'no' ]; then
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [ERROR] Failed to download"
+                rm -f $DOWNLOAD
+        fi
    exit 1
 fi
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Extracting $DOWNLOAD"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Extracting $DOWNLOAD"
 fi
 tar -xf $DOWNLOAD
 
@@ -125,13 +132,13 @@ if [ $? -ne 0 ]; then
 fi
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Shuffling directories"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Shuffling directories"
 fi
 mv $WORKING_DIR $BACKUP_DIR
 mv $TEMP_DIR $WORKING_DIR
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Copying over files from $BACKUP_DIR"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Copying over files from $BACKUP_DIR"
 fi
 
 if [ -f $STORAGE_DIR/database.json ]; then
@@ -177,22 +184,22 @@ fi
 cp -rn $BACKUP_DIR/wwwroot/images/* $WORKING_DIR/wwwroot/images || true
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Changing ownership to $SERVICE_NAME"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Changing ownership to $SERVICE_NAME"
 fi
 chown -R $SERVICE_NAME:$SERVICE_NAME $WORKING_DIR
 
 if [ $KEEP_BACKUP == "yes" ]; then
-	if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Keeping $BACKUP_DIR"
-	fi
+        if [ $SUPPRESS_OUTPUT = 'no' ]; then
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Keeping $BACKUP_DIR"
+        fi
 elif [ $KEEP_BACKUP == "no" ]; then
-	if [ $SUPPRESS_OUTPUT = 'no' ]; then
-		echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Deleting $BACKUP_DIR"
-	fi
+        if [ $SUPPRESS_OUTPUT = 'no' ]; then
+                echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Deleting $BACKUP_DIR"
+        fi
    rm -rf $BACKUP_DIR
 fi
 
 if [ $SUPPRESS_OUTPUT = 'no' ]; then
-	echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Starting $SERVICE_NAME"
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N") [INFO] Starting $SERVICE_NAME"
 fi
 systemctl start $SERVICE_NAME
